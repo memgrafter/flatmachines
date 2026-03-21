6bd9d86dc80be310aecc869c39265d4c0be8c3ce
memgrafter <memgrafter@gmail.com>
2026-03-20 20:38:30 -0700

    bump version

--- sdk/python/flatmachines/flatmachines/__init__.py
+++ sdk/python/flatmachines/flatmachines/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "2.4.0"
+__version__ = "2.4.1"
 
 from .flatmachine import FlatMachine
 from .hooks import MachineHooks, LoggingHooks, MetricsHooks, CompositeHooks, WebhookHooks, HooksRegistry
